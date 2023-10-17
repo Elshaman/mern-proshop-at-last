e68be83abb5c593b1f6ff03c189ca15a8e54047b
Elshaman <cbuitragoo.2023@gmail.com>
2023-10-17 18:39:31 -0500

    react router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Navbar , Nav , Container, NavbarToggle, NavbarCollapse, NavLink } from  'react-bootstrap'
 import { FaShoppingCart , FaUser }  from 'react-icons/fa'
 import logo from '../assets/logo.png'
+import { LinkContainer } from 'react-router-bootstrap'
 
 
 const Header = () => {
@@ -8,17 +9,29 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/" >
-                    <img src={logo} alt="Proshop" />
-                    Proshop
-                </Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand >
+                        <img src={logo} alt="Proshop" />
+                        Proshop
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav.Link href="/cart" >
-                            <FaShoppingCart />
-                                Cart
-                        </Nav.Link>
+                        <LinkContainer to="/cart">
+                            <Nav.Link>
+                                <FaShoppingCart />
+                                    Cart
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link>
+                                <FaUser />
+                                    Sing In
+                            </Nav.Link>
+                        </LinkContainer>
+                        
+                       
                     </Nav>
                 </Navbar.Collapse>
             </Container>
