d392552a7d3e0e2ad9b619c55db05cf2c7144a8f
Elshaman <cbuitragoo.2023@gmail.com>
2023-10-22 09:59:50 -0500

    data seeder

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose"
+import colors from 'colors'
 
 const connectDB = async() => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.bgYellow.magenta)
     } catch (error) {
         console.log(`Error:${error.message}`)
         process.exit(1)
     }
 }
 
-
+//connectDB()
 export default connectDB;
