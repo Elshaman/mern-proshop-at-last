a5fb0f854a8453cc12acef1c7e49e9d97281b1b0
Elshaman <cbuitragoo.2023@gmail.com>
2023-10-24 11:03:45 -0500

    product slice sin axios

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,27 +1,25 @@
-import { useEffect, useState } from 'react'
+// ya no necesitamos los hooks gracias a redux : import { useEffect, useState } from 'react'
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 //import products from '../_data/products'
 import Product from '../components/Product'
-import axios from 'axios'
+//ya no necesitamos axios gracias al fetch de api slice : import axios from 'axios'
 
-
+import { useGetProductsQuery } from '../_redux/slices/productApiSlice'
 
 const HomeScreen = () => {
 
-  const [products, setProducts] = useState([])  
+  //ya no necesitamos estado de componente gracias a redux const [products, setProducts] = useState([])  
 
-  useEffect(()=>{
-      const fetchProducts = async() =>{
-        const {data} = await axios.get('/api/products')
-        setProducts(data)
-      }
-
-      fetchProducts()
-  })
-
+  const {data: products , isLoading , error } = useGetProductsQuery()
   return (
     <>
+    {isLoading ? (
+            <h2>Loading...</h2>
+    ) : error ? ( <div>
+            <div>{error?.data?.message || error.error }</div>
+    </div>) : (
+      <>
         <h1>Latest Products</h1>
         <Row>
             { products.map((product) => (
@@ -30,6 +28,11 @@ const HomeScreen = () => {
                 </Col>
             ) )}
         </Row>
+      </>
+
+
+    )}
+        
     </>
   )
 }
