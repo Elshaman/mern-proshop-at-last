77cb7e2b27a1b704ee29178d019aebeece209cd9
Elshaman <cbuitragoo.2023@gmail.com>
2023-10-24 05:40:04 -0500

    custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 //import products from './_data/products.js'
 
 dotenv.config();
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 
@@ -17,7 +19,8 @@ app.get('/' , (req, res)=>{
     res.send("API is running")
 })
 
-
+app.use(notFound)
+app.use(errorHandler)
 
 
 app.listen(port , () => console.log(`Server running on port ${port}`))
