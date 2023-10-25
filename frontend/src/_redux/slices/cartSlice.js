3f32ce8770b7d773cdb5427c63a2fd597a87cfc0
Elshaman <cbuitragoo.2023@gmail.com>
2023-10-25 12:10:57 -0500

    function add to cart

--- frontend/src/_redux/slices/cartSlice.js
+++ frontend/src/_redux/slices/cartSlice.js
@@ -5,11 +5,49 @@ const initialState = localStorage.getItem("cart") ?
                          : 
                         {cartItems: []}
 
+const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2)
+}
 
 const cartSlice = createSlice({
     name:"cart",
     initialState,
-    reducers:{}
+    reducers:{
+        addToCart: ( state, action) =>{
+            //trae el item del body 
+            const item = action.payload
+            //mira si el item esta en el carrito: cartITems
+            const existItem = state.cartItems.find((x)=>x._id === item._id)
+            if(existItem){
+                //item existente
+                state.cartItems = state.cartItems.map((x)=>x._id === existItem._id? item : x)
+            }else{
+                //nuevo item: se añade al carrito
+                state.cartItems = [ ...state.cartItems, item ]
+            }
+
+            //calculate items price
+            state.itemsPrice = addDecimals(state.cartItems.reduce((acc , item ) => acc + item.price * item.qty , 0))
+
+            //calculate shipping price
+            //if order id over $100 then free, else $10 shpipping
+            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+
+            //calculate tax price(15% tax)
+            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
+
+            //calculate total price:
+            state.totalPrice = (
+                Number(state.itemsPrice) + 
+                Number(state.shippingPrice) + 
+                Number(state.taxPrice)
+            ).toFixed(2)
+
+            localStorage.setItem('cart' , JSON.stringify(state))
+
+        }
+    }
 })
 
+export const {addToCart } = cartSlice.actions
 export default cartSlice.reducer
